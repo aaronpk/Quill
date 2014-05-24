306ffeba189e79af47d95d35c5c6a9d99ad317a5
Aaron Parecki <aaron@parecki.com>
2014-05-24 15:22:36 -0700

    fix typo

--- views/docs.php
+++ views/docs.php
@@ -4,9 +4,13 @@
   <span class="thumbnail"><img src="/images/indiepost-ui.png"></span>
 </div>
 
-<p>This is a simple <a href="http://indiewebcamp.com/micropub">Micropub</a> client for creating text posts on your own website. To use it, you will need to turn your website into an OAuth provider, and implement a Micropub endpoint that this app will send requests to.</p>
+<p>This is a simple <a href="http://indiewebcamp.com/micropub">Micropub</a> client for 
+   creating text posts on your own website. To use it, you will need to turn your website 
+   into an OAuth provider, and implement a Micropub endpoint that this app will send 
+   requests to.</p>
 
-<p>Once you've signed in, you'll see an interface like the below which you can use to write a post. Clicking "post" will make a Micropub request to your endpoint.<p>
+<p>Once you've signed in, you'll see an interface like the one shown which you can use to 
+   write a post. Clicking "post" will make a Micropub request to your endpoint.<p>
 
 <h2>Configuring Endpoints</h2>
 
