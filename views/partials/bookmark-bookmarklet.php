d6470f1872f12b09a130a818c2818ee3389461c8
Aaron Parecki <aaron@parecki.com>
2014-09-07 12:18:38 -0700

    try the quote thing again

--- views/partials/bookmark-bookmarklet.php
+++ views/partials/bookmark-bookmarklet.php
@@ -1,5 +1,5 @@
 javascript:(function(){
   var t;try{t=((window.getSelection&&window.getSelection())||(document.getSelection&&document.getSelection())||(document.selection&&document.selection.createRange&&document.selection.createRange().text));}catch(e){t="";};
-  window.location="<?= Config::$base_url ?>bookmark?url="+encodeURIComponent(window.location.href)+"&content="+encodeURIComponent((t ? '"'+t+'"' : ''))+"&name="+encodeURIComponent(document.title)+"&token=<?= $this->token ?>";
+  window.location="<?= Config::$base_url ?>bookmark?url="+encodeURIComponent(window.location.href)+"&content="+encodeURIComponent((t == '' ? '' : '"'+t+'"'))+"&name="+encodeURIComponent(document.title)+"&token=<?= $this->token ?>";
 })();
 
