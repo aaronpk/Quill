37c516216ef0321d63bdc1ba2683fb9b3e6b6e45
Aaron Parecki <aaron@parecki.com>
2014-09-07 11:58:42 -0700

    get bookmarklet base url from config

--- lib/config.template.php
+++ lib/config.template.php
@@ -1,6 +1,7 @@
 <?php
 class Config {
   public static $hostname = 'quill.dev';
+  public static $base_url = 'http://quill.dev/';
   public static $gaid = '';
 
   public static $dbHost = '127.0.0.1';
--- views/partials/bookmark-bookmarklet.php
+++ views/partials/bookmark-bookmarklet.php
@@ -1,5 +1,5 @@
 javascript:(function(){
   var t;try{t=((window.getSelection&&window.getSelection())||(document.getSelection&&document.getSelection())||(document.selection&&document.selection.createRange&&document.selection.createRange().text));}catch(e){t="";};
-  window.location="http://quill.dev/bookmark?url="+encodeURIComponent(window.location.href)+"&content="+encodeURIComponent('"'+t+'"')+"&name="+encodeURIComponent(document.title)+"&token=<?= $this->token ?>";
+  window.location="<?= Config::$base_url ?>bookmark?url="+encodeURIComponent(window.location.href)+"&content="+encodeURIComponent('"'+t+'"')+"&name="+encodeURIComponent(document.title)+"&token=<?= $this->token ?>";
 })();
 
