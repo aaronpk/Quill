de12b9ef24ab43e9241ff3c37162b5204792fd02
Aaron Parecki <aaron@parecki.com>
2016-03-16 09:34:10 -0400

    icons for dashboard

--- views/dashboard.php
+++ views/dashboard.php
@@ -1,16 +1,26 @@
 <div class="narrow">
   <?= partial('partials/header') ?>
 
-  <ul style="margin-top: 1em;">
-    <li><a href="/editor">Editor</a></li>
-    <li><a href="/new">Note</a></li>
-    <li><a href="/event">Event</a></li>
-    <li><a href="/bookmark">Bookmark</a></li>
-    <li><a href="/favorite">Favorite</a></li>
-    <li><a href="/repost">Repost</a></li>
-    <li><a href="/photo">Photo</a></li>
-    <li><a href="/itinerary">Itinerary</a></li>
-    <li><a href="/email">Email</a></li>
+  <ul class="post-type-icons">
+    <li><a href="/editor"><img src="/images/quill.svg" width="60"></a></li>
+    <li><a href="/new"><img src="/images/note.svg" width="60"></a></li>
+    <li><a href="/event"><img src="/images/calendar.svg" width="60"></a></li>
+    <li><a href="/bookmark"><img src="/images/bookmark.svg" width="60"></a></li>
+    <li><a href="/favorite"><img src="/images/star.svg" width="60"></a></li>
+    <li><a href="/repost"><img src="/images/repost.svg" width="60"></a></li>
+    <li><a href="/photo"><img src="/images/camera.svg" width="60"></a></li>
+    <li><a href="/itinerary"><img src="/images/plane.svg" width="60"></a></li>
+    <li><a href="/email"><img src="/images/email.svg" width="60"></a></li>
   </ul>
 
 </div>
+<style type="text/css">
+.post-type-icons {
+  margin-top: 1em; 
+  list-style-type: none;
+}
+.post-type-icons li {
+  float: left;
+  margin-right: 12px;
+}
+</style>
